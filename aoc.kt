7212bb9c9e5eda735e3086aa9e0403b879d5eec6
Powah <federico.scloza@mail.polimi.it>
2021-12-02 23:42:35 +0100

    Day 2

--- aoc.kt
+++ aoc.kt
@@ -32,6 +32,47 @@ fun day1() {
     println(helper(input, 3))
 }
 
-fun main() {                        
-    day1();
+fun day2() {
+    
+    class ParseLine(line: String) {
+        val command = line.split(" ").first()
+        val amount = line.split(" ").last().toInt()
+        operator fun component1(): String = command
+        operator fun component2(): Int = amount
+    }
+
+    val input = readlinesFromDay(2);
+    
+    // Part 1    
+    var pos: Pair<Int, Int> = Pair(0, 0)
+
+    for (line: String in input) {
+        var (command, amount) = ParseLine(line)
+        when (command) {
+            "forward" -> pos = pos.copy(first = pos.first + amount)
+            "down" -> pos = pos.copy(second = pos.second + amount)
+            "up" -> pos = pos.copy(second = pos.second - amount)
+        }
+    }
+
+    println(pos.first * pos.second)
+    // Part 2
+    pos = Pair(0, 0)
+    var aim: Int = 0
+
+    for (line: String in input) {
+        var (command, amount) = ParseLine(line)
+        when (command) {
+            "forward" -> pos = Pair(pos.first + amount, pos.second + (aim * amount))
+            "down" -> aim += amount
+            "up" -> aim -= amount
+        }
+    }
+    
+    println(pos.first * pos.second)
+}
+
+fun main() {      
+    // day1();                  
+    day2();
 }
