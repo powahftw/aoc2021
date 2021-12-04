116a1e6f93eaafc92538d8d0ec92f1e7569f9514
Powah <federico.scloza@mail.polimi.it>
2021-12-04 11:33:59 +0100

    Day 4 + Style fixes

--- aoc.kt
+++ aoc.kt
@@ -5,7 +5,7 @@ import java.io.File
 fun readlinesFromDay(day: Int): List<String> {
     val file = File("in/${day}.txt")
     if (!file.exists()) { 
-        throw Exception("Missing input file for day ${day}")
+        throw Exception("Missing input file for day $day")
     }
     return file.readLines()
 }
@@ -47,7 +47,7 @@ fun day2() {
     var pos: Pair<Int, Int> = Pair(0, 0)
 
     for (line: String in input) {
-        var (command, amount) = ParseLine(line)
+        val (command, amount) = ParseLine(line)
         when (command) {
             "forward" -> pos = pos.copy(first = pos.first + amount)
             "down" -> pos = pos.copy(second = pos.second + amount)
@@ -61,7 +61,7 @@ fun day2() {
     var aim: Int = 0
 
     for (line: String in input) {
-        var (command, amount) = ParseLine(line)
+        val (command, amount) = ParseLine(line)
         when (command) {
             "forward" -> pos = Pair(pos.first + amount, pos.second + (aim * amount))
             "down" -> aim += amount
@@ -74,9 +74,7 @@ fun day2() {
 
 fun day3() {
 
-    fun convertAndMultiply(first: String, other: String): Int {
-        return listOf(first, other).map { it -> Integer.parseInt(it, 2)}.reduce { acc, next -> acc * next }
-    }
+    fun String.bitToInt() = Integer.parseInt(this, 2)
 
     fun commonBit(candidates: List<String>, idx: Int, preferHigh: Boolean): Char {
         var count: Map<Char, Int> = candidates.map { it -> it.get(idx) }. groupingBy { it }.eachCount()
@@ -84,38 +82,118 @@ fun day3() {
         if (count.getOrDefault('1', 0) > count.getOrDefault('0', 0)) { return if (preferHigh) '1' else '0' }
         return if (preferHigh) '0' else '1'
     }
-    val input = readlinesFromDay(3);
-    var gamma_rate = ""
+    val input = readlinesFromDay(3)
+    var gammaRateS = ""
     for (idx: Int in input[0].indices) {
-        var sum = 0
-        for (line: String in input) {
-            sum += line[idx].toString().toInt()
-        }
-        gamma_rate += if ((sum.toDouble() / input.size) > 0.5) "1" else "0"
+        gammaRateS += commonBit(input, idx, false)
     }
-    var epsilon_rate = gamma_rate. map { it -> if (it == '0') "1" else "0" }.joinToString(separator = "")
-    var power = convertAndMultiply(epsilon_rate, gamma_rate)
+    var gammaRate = gammaRateS
+    var epsilonRate = gammaRateS.map { it -> if (it == '0') "1" else "0" }.joinToString(separator = "")
+    var power = epsilonRate.bitToInt() * gammaRate.bitToInt()
     println(power)
 
-    var co2_candidates: List<String> = input.toMutableList()
-    var o2_candidates: List<String> = input.toMutableList()
+    var co2Candidates: List<String> = input.toMutableList()
+    var o2Candidates: List<String> = input.toMutableList()
     for (idx: Int in input[0].indices) {
-        if (o2_candidates.size > 1) {
-            var max_common = commonBit(o2_candidates, idx, true)
-            o2_candidates = o2_candidates.filter { it -> it.get(idx) == max_common} 
+        if (o2Candidates.size > 1) {
+            val maxCommon = commonBit(o2Candidates, idx, true)
+            o2Candidates = o2Candidates.filter { it -> it[idx] == maxCommon}
         }
-        if (co2_candidates.size > 1) {
-            var min_common = commonBit(co2_candidates, idx, false)
-            co2_candidates = co2_candidates.filter { it -> it.get(idx) == min_common} 
+        if (co2Candidates.size > 1) {
+            val minCommon = commonBit(co2Candidates, idx, false)
+            co2Candidates = co2Candidates.filter { it -> it[idx] == minCommon}
         }
     }
-    var life_rating = convertAndMultiply(co2_candidates.get(0), o2_candidates.get(0))
-    println(life_rating)
+    val lifeRating = co2Candidates[0].bitToInt() * o2Candidates[0].bitToInt()
+    println(lifeRating)
     
 }
 
-fun main() {      
-    // day1();                  
-    // day2();
-    day3();
+fun day4() {
+    
+    fun MutableList<MutableList<Int>>.tickNumber(n: Int) {
+        for (idx_r in this.indices) {
+            for (idx_c in this[idx_r].indices) {
+                if (this[idx_r][idx_c] == n) {
+                    this[idx_r][idx_c] = -1
+                    }
+            }
+        }
+    }
+
+    fun MutableList<MutableList<Int>>.isWinner(): Boolean {
+        for (line in this) {
+            if (line.sum() == -5) {
+                return true
+            }
+        }
+        
+        for (idx_c in this[0].indices) {
+            var sumCol = 0
+            for (idx_r in this.indices) {
+                sumCol += this[idx_r][idx_c]
+            }
+            if (sumCol == -5) {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun MutableList<MutableList<Int>>.sumM(): Int {
+        var totSum = 0
+        for (line in this) {
+            totSum += line.sumOf { it -> if (it == -1) 0 else it }
+        }
+        return totSum
+    }
+
+    val input = readlinesFromDay(4)
+    val extractedNumbers = input.first().split(",").map { it -> it.toInt() }
+    val bingoCards: MutableMap<Int, MutableList<MutableList<Int>>> = mutableMapOf<Int, MutableList<MutableList<Int>>>()
+    var currentBingoCardN = 0
+    var currentCard: MutableList<MutableList<Int>> = mutableListOf<MutableList<Int>>()
+    var numFoundInCard: MutableMap<Int, MutableList<Int>> = mutableMapOf<Int, MutableList<Int>>()
+    for (line in input.slice(3 until input.size)) {
+        if (line.isEmpty()) {
+            currentBingoCardN++
+            bingoCards[currentBingoCardN] = currentCard
+            currentCard = mutableListOf<MutableList<Int>>()
+        }
+        else {
+            val foundNumbers = line.trim().split(" +".toRegex()).map { it -> it.toInt() }.toMutableList()
+            currentCard.add(foundNumbers)
+        }
+    }
+    currentBingoCardN++
+    bingoCards[currentBingoCardN] = currentCard
+    println(bingoCards)
+
+    fun extractAndFindWinnerSum(extractedNumbers: List<Int>, bingoCards: MutableMap<Int, MutableList<MutableList<Int>>>) {
+        val winners: MutableSet<Int> = mutableSetOf<Int>()
+        for (extractedNumber in extractedNumbers) {
+            for ((key, bingoCard) in bingoCards) {
+                if (winners.contains(key)) { continue }
+                bingoCard.tickNumber(extractedNumber)
+                if (bingoCard.isWinner()) {
+                    winners.add(key)
+                    if (winners.size == 1 || winners.size == bingoCards.size) {
+                        println(bingoCard.sumM() * extractedNumber)
+                    }
+                    if (winners.size == bingoCards.size) {
+                        return
+                    }
+                }
+            }
+        }
+    }
+
+    extractAndFindWinnerSum(extractedNumbers, bingoCards)
+}
+
+fun main() {     
+    // day1()              
+    // day2()
+    day3()
+    // day4()
 }
